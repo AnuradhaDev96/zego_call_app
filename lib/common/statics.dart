5bce6facb0423f8dc1f876401f551c02319950f8
Anuradha Sampath Hewa Siribaddana <anusampath9470@gmail.com>
2023-04-16 18:38:08 +0530

    Stripe payment sheet implementation

--- lib/common/statics.dart
+++ lib/common/statics.dart
@@ -1,4 +1,6 @@
 class Statics {
   static const String zegoAppSign = 'ea7f4dedfd9b8b3d31fb6640c30dc0b4ece7a773230327404f1a30eff446e68c';
   static const int zegoAppId = 1803218513;
+  static const String stripePublishableKey = 'pk_test_51MxBryJKjNVqOWzSmGYcqVAkiduiZh6AopEtAnDI5CwNJmQannIA5zTBqj0cs3nL6CF7iWbeaPpaq9CUBkoLDggM00xF8sgaUc';
+  static const String stripeSecretKey = 'sk_test_51MxBryJKjNVqOWzSes3KcA9dUcqMvuybpQ0xzIY24W44oUghwRMQVi1beqf6jYeKO4lPCITsy698kHaH3DtRR8hB00Ud1UxxxW';
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,20 @@
 import 'package:awesome_notifications/awesome_notifications.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:provider/provider.dart';
 import 'package:push_notifications_app/firebase_options.dart';
 import 'package:zego_uikit_prebuilt_call/zego_uikit_prebuilt_call.dart';
 
 import 'change_notifiers/call_state_change_notifier.dart';
 import 'common/dependency_locator.dart';
+import 'common/statics.dart';
 import 'message_handler.dart';
-import 'models/user_model.dart';
 import 'services/firebase_service.dart';
-import 'zego/call_history_page.dart';
+import 'zego/dashboard_page.dart';
 import 'zego/login_page.dart';
-import 'zego/user_card.dart';
 
 // final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
@@ -41,6 +39,8 @@ Future<void> main() async {
   injectDependencies();
 
   FirebaseMessaging.onBackgroundMessage(MessageHandler.firebaseMessagingBackgroundHandler);
+
+  Stripe.publishableKey = Statics.stripePublishableKey;
 
   // await flutterLocalNotificationsPlugin
   //     .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
@@ -116,180 +116,4 @@ class _MyAppState extends State<MyApp> {
       ),
     );
   }
-}
-
-class DashboardPage extends StatelessWidget {
-  const DashboardPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Sam Caller"),
-        actions: [
-          IconButton(
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (_) => const CallHistoryPage()));
-            },
-            icon: const Icon(Icons.history, size: 20.0),
-          ),
-          IconButton(
-            onPressed: () {
-              FirebaseService.logout();
-            },
-            icon: const Icon(Icons.logout, size: 20.0),
-          ),
-        ],
-      ),
-      body: Consumer<CallStateChangeNotifier>(
-          builder: (BuildContext context, CallStateChangeNotifier callStatusNotifier, child) {
-        return Stack(
-          children: [
-            Center(
-              child: StreamBuilder<QuerySnapshot>(
-                stream: FirebaseService.buildViews,
-                builder: (context, snapshot) {
-                  if (!snapshot.hasData) {
-                    return const Center(child: CircularProgressIndicator());
-                  }
-
-                  final List<QueryDocumentSnapshot>? documents = snapshot.data?.docs;
-
-                  if (documents == null || documents.isEmpty) {
-                    return const Text("No Data");
-                  }
-
-                  return ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: documents.length,
-                    itemBuilder: (context, index) {
-                      final model = UserModel.fromMap(documents[index].data() as Map<String, dynamic>);
-                      if (!FirebaseService.isCurrentUser(model.email)) {
-                        return UserCard(userModel: model);
-                      }
-                      return const SizedBox.shrink();
-                    },
-                  );
-                },
-              ),
-            ),
-            Visibility(
-              visible: callStatusNotifier.isIncomingCall,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 200.0),
-                child: Card(
-                  elevation: 8.0,
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.0)),
-                  color: const Color(0xFF2D3B29),
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 40.0),
-                    child: Column(
-                      children: [
-                        const Text(
-                          "Incoming Call",
-                          style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold, color: Colors.white),
-                        ),
-                        const SizedBox(height: 20.0),
-                        CircleAvatar(
-                          backgroundColor: Theme.of(context).primaryColor,
-                          radius: 35.0,
-                          child: Center(
-                            child: Text(
-                              callStatusNotifier.caller == null
-                                  ? "N/A"
-                                  : callStatusNotifier.caller!.name.substring(0, 1).toUpperCase(),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 15.0),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              callStatusNotifier.caller == null ? "N/A" : callStatusNotifier.caller!.name,
-                              style: const TextStyle(
-                                fontSize: 18.0,
-                                // fontWeight: FontWeight.bold,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ],
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ],
-        );
-      }),
-    );
-  }
-
-// @override
-// Widget build(BuildContext context) {
-//   return FutureBuilder<UserModel?>(
-//       future: FirebaseService.currentUser,
-//       builder: (context, snapshot) {
-//         if (snapshot.connectionState == ConnectionState.waiting) {
-//           return const Center(child: CircularProgressIndicator());
-//         }
-//
-//         if (snapshot.hasData) {
-//           UserModel? currentUser = snapshot.data;
-//
-//           if (currentUser == null) {
-//             return const Center(child: Text("Problem in authorization. Please login again"));
-//           }
-//           return CallInvitationPage(
-//             username: currentUser.username,
-//             child: Scaffold(
-//               appBar: AppBar(
-//                 title: const Text("Sam Caller"),
-//                 actions: [
-//                   IconButton(
-//                     onPressed: () {
-//                       FirebaseService.logout();
-//                     },
-//                     icon: const Icon(Icons.logout, size: 20.0),
-//                   ),
-//                 ],
-//               ),
-//               body: Center(
-//                 child: StreamBuilder<QuerySnapshot>(
-//                   stream: FirebaseService.buildViews,
-//                   builder: (context, snapshot) {
-//                     if (!snapshot.hasData) {
-//                       return const Center(child: CircularProgressIndicator());
-//                     }
-//
-//                     final List<QueryDocumentSnapshot>? documents = snapshot.data?.docs;
-//
-//                     if (documents == null || documents.isEmpty) {
-//                       return const Text("No Data");
-//                     }
-//
-//                     return ListView.builder(
-//                       shrinkWrap: true,
-//                       itemCount: documents.length,
-//                       itemBuilder: (context, index) {
-//                         final model = UserModel.fromMap(documents[index].data() as Map<String, dynamic>);
-//                         if (!FirebaseService.isCurrentUser(model.email)) {
-//                           return UserCard(userModel: model);
-//                         }
-//                         return const SizedBox.shrink();
-//                       },
-//                     );
-//                   },
-//                 ),
-//               ),
-//             ),
-//           );
-//         }
-//
-//         return const Center(child: Text("Please wait. We are setting things up."));
-//       }
-//   );
-// }
 }
